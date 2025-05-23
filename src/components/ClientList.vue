8db85fa88b91ad7594e5fd41a422297ebb12e793
seth1nk <kostik.vs10@mail.ru>
2025-05-23 21:39:09 +0800

    Update ClientList.vue

--- src/components/ClientList.vue
+++ src/components/ClientList.vue
@@ -63,7 +63,7 @@ export default {
       currentPage: 1,
       totalPages: 0,
       pageSize: 10,
-      backendUrl: 'http://localhost:3000',
+      backendUrl: 'https://zvue.onrender.com',
     };
   },
   async created() {
